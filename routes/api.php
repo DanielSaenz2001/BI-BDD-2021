9e76e42d6884590480958d674820ac40ad967e39
DanielSaenz2001 <daniel.saenz@upeu.edu.pe>
2021-05-25 12:28:42 -0500

    api routes productor

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,15 @@
 <?php
 
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Authorization,Origin, Content-Type, X-Auth-Token, X-XSRF-TOKEN');
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +21,18 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+    'middleware' => 'api',
+    'namespace'  => 'App\Http\Controllers',
+], function () {
+        //-----------------------API-JWT------------------------\\
+        Route::post('login', 'ProductorController@login');
+        Route::post('changePassword', 'ProductorController@cambiarContra');
+        Route::get('logout', 'ProductorController@logout');
+        Route::get('me', 'ProductorController@me');
+        Route::get('productor', 'ProductorController@index');
+        Route::get('productor/{id}', 'ProductorController@show');
+        Route::post('productor', 'ProductorController@create');
+        Route::put('productor/{id}', 'ProductorController@update');
 });
+    
